adb47e7a6b33a1767236d3c88e4be54b51fbd862
RostilKant <rostilnytskiy@gmail.com>
2021-04-06 19:58:23 +0300

    Initial commit

---
+++ SolityTest/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SolityTest.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CORS", builder =>
+                        builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                );
+            });
+    }
+}
